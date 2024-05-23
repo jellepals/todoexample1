f69528434efa25d467b949baebc40734b00d66ed
Jelle Pals <jellepals@Jelles-MacBook-Pro.local>
2024-05-23 15:06:13 +0200

    implement delete button

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ Route::get('/', function () {
     return view('welcome',['todos' => Todo::all()]);
 });
 
-Route::get('/delete/{id}', function($id) {
-    
-});
+Route::get('/delete/{todo}', function(Todo $todo) {
+    $todo->delete();
+    return redirect('/');
+})->name('delete');
 
 Route::post('/post', function() {
     Todo::create(['todo' => request('todo')]);
